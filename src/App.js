ba8948c52e1955c35830b7e8812af3ca413a24dd
Mohammed <61502453+mk-Guvi@users.noreply.github.com>
2021-08-28 09:51:21 -0400

    store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import CakeContainer from "./components/CakeContainer";
+import { Provider } from "react-redux";
+import store from "./redux/cake/store";
 import "./styles.css";
+//to provide redux store to react application react-redux exports an component called provider.
 
 export default function App() {
   return (
-    <div className="App">
-      <CakeContainer />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer />
+      </div>
+    </Provider>
   );
 }
