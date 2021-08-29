3c013a2352020752129a8756efa10d842f1a4db2
Mohammed <61502453+mk-Guvi@users.noreply.github.com>
2021-08-28 23:34:00 -0400

    dispatchHook

--- src/components/HookCakeContainer.js
+++ src/components/HookCakeContainer.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { buyCake } from "../redux"; //actionCreator
 
 function HookCakeContainer() {
   //it is the replacement of mapStateToProps.It accepts a function as parameter.This function is called as selector function.selector function receives the redux state as its argument which is similar to mapStateToProps function.the selector function returns a value that is state.numOfCakes.
   const numOfCakes = useSelector((state) => state.numOfCakes);
+  const dispatch = useDispatch(); //it allows to use dispatch action of redux
   return (
     <div>
       <h2>HookContainer</h2>
       <h2>Number of cakes-{numOfCakes}</h2>
-      <button>Buy Cake</button>
+      <button onClick={() => dispatch(buyCake())}>Buy Cake</button>
     </div>
   );
 }
