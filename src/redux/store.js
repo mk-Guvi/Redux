871dc99e37262b87ef9658bd606ae53b98ebe833
Mohammed <61502453+mk-Guvi@users.noreply.github.com>
2021-08-29 22:24:48 -0400

    loggerMiddleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
-import { createStore } from "redux";
-import cakeReducer from "./cake/cakeReducers";
+import { createStore, applyMiddleware } from "redux";
+//import cakeReducer from "./cake/cakeReducers";
+import logger from "redux-logger";
 import rootReducer from "./rootReducer";
 //create strore can acceot only one reducer
 
 //const store = createStore(cakeReducer);
 //for multiple reducers we use rootReducer
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
