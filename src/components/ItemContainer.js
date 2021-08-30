d797e23b1f7e8617b75eca4d8e8c4ab940d9792a
Mohammed <61502453+mk-Guvi@users.noreply.github.com>
2021-08-29 23:21:13 -0400

    onwProps-mapsDispatchToProps

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
+import { buyCake, buyIceCream } from "../redux/index";
 
 function ItemContainer(props) {
   return (
     <div>
       <h2>ItemContainer</h2>
       <h3>Item -{props.item}</h3>
+      <button onClick={props.buyItem}>Buy Item</button>
     </div>
   );
 }
@@ -19,4 +21,16 @@ const mapStateToProps = (state, ownProps) => {
     item: itemState
   };
 };
-export default connect(mapStateToProps)(ItemContainer);
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchFunction = ownProps.cake
+    ? () => dispatch(buyCake())
+    : () => dispatch(buyIceCream());
+
+  return {
+    buyItem: dispatchFunction //it allows us to map the actionCreators to prop in our components
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(ItemContainer);
+
+//if you want only dispatch action but not subscribe to the state changes in the store then null is passed
+// export default connect(null,mapDispatchToProps)(ItemContainer);
